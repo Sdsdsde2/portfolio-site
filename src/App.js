9e47e8dddb63103d63548185e17ef55883c74ced
Sdsdsde2 <imspammails31@gmail.com>
2021-07-16 09:01:51 -0500

    added major css and styling changes. added typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-import pfp from './pfpexample.png';
+import pfp from './portfoliopfp.jpg';
 import anime from 'animejs/lib/anime.es.js';
 
+var i = 0;
+var j = 0;
+var k = 0;
+var nameTxt = 'Shane Rachal';
+var infoTxt = 'Full-Stack Web Developer';
+var thanksTxt = 'Website';
+
 export default class App extends Component {
+  typingStart = () => {
+    if (i < nameTxt.length) {
+      document.getElementsByClassName("App-description-name")[0].innerHTML += nameTxt.charAt(i);
+      i++;
+      setTimeout(this.typingStart, 100);
+    }
+    else if (j < infoTxt.length) {
+      document.getElementsByClassName("App-description-info")[0].innerHTML += infoTxt.charAt(j);
+      j++;
+      setTimeout(this.typingStart, 100);
+    }
+    else if (k < thanksTxt.length) {
+      document.getElementsByClassName("App-description-thanks")[0].innerHTML += thanksTxt.charAt(k);
+      k++;
+      setTimeout(this.typingStart, 100);
+    }
+    else {
+      this.animationStart();
+    }
+  }
+
   animationStart() {
     for(var i = 1; i < 6; i++) {
       // eslint-disable-next-line no-loop-func
@@ -39,14 +67,11 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.animationStart();
+    this.typingStart();
   }
 
   handleClick(e) {
     e.preventDefault();
-    // console.log(e)
-    // console.log(e.target.id)
-    // NOTE TO FUTURE SELF: When I click on the text i.e "Github" it doesn't redirect.
     if (e.target.id === "link-5") {
       window.location.href="https://github.com/Sdsdsde2"
     }
@@ -72,18 +97,49 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1 className="App-title">
+            Shane Possibly
+          </h1>
           <div className="App-pfp-boarder">
             <img src={pfp} className="App-pfp"></img>
           </div>
           <div>
-            <h1 className="App-title">
-              Shane Possibly
-            </h1>
+            <span className="App-description">
+              {"Hello, my name is "}
+              <span className="App-description-name">
+                {/* Filled in with typewriter */}
+              </span>
+              .
+            </span>
+          </div>
+          <div>
+            <span>
+              {"I am a "}
+              <span className="App-description-info">
+                {/* Filled in with typewriter */}
+              </span>
+              .
+            </span>
+          </div>
+          <div>
+            <span>
+              {"Thank you for visiting my "}
+              <span className="App-description-thanks">
+                {/* Filled in with typewriter */}
+              </span>
+              .
+            </span>
+          </div>
+          <div>
             <input type="button" className="App-link" id="link-5" onClick={this.handleClick} value=" Github "/>
             <input type="button" className="App-link" id="link-4" onClick={this.handleClick} value="YouTube"/>
             <input type="button" className="App-link" id="link-3" onClick={this.handleClick} value="Twitter"/>
             <input type="button" className="App-link" id="link-2" onClick={this.handleClick} value="Linkedin"/>
             <input type="button" className="App-link" id="link-1" onClick={this.handleClick} value="Medium"/>
+          </div>
+          <div>
+            <h3 className="Section-titles">About Me</h3>
+
           </div>
         </header>
       </div>
